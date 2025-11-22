afcde590bcbbaf2872025f63bcb6cf4fc50c2f51
Ahmed Abdo <devahmedabdo@gmail.com>
2025-11-22 14:00:12 +0200

    first commit

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'attendance-cache-v1';
+const FILES = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
